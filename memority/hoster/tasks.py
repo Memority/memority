fee87aa85e3c5dcbe361741fd0f84ffd1ff45007
AndriyVityk <andrii.vityk@gmail.com>
2018-06-12 14:39:33 +0300

    Celery eventlet pool (for windows support)

--- memority/hoster/tasks.py
+++ memority/hoster/tasks.py
@@ -133,6 +133,6 @@ def create_celery_processes():
         ),
         mp.Process(  # start worker
             target=main_,
-            args=[['_', '-A', 'hoster.tasks', 'worker', '--loglevel=info', '-E']]
+            args=[['_', '-A', 'hoster.tasks', 'worker', '--loglevel=info', '--pool=eventlet', '-E']]
         )
     ]
