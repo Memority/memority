e8c2bedc1a914920753e69b4beafdb856e098c7b
AndriyVityk <andrii.vityk@gmail.com>
2018-06-14 12:05:35 +0300

    upd contract addresses

--- memority/smart_contracts/compile.py
+++ memority/smart_contracts/compile.py
@@ -53,8 +53,8 @@ def main():
     res['Token'][0]["address"] = '0x8C6beb352014dA46Ba85B5164f0b95DAEF5375d5'
     res['MemoDB'][0]["address"] = '0x46FDE65ce40E753B08106560E2Bc82eb28715198'
 
-    res['Token'][1000]["address"] = '0x124D1f206646Fe9C26693981459471b893be286c'
-    res['MemoDB'][1000]["address"] = '0x27C823b254C74989201cEc2A9db6eBbBf169eED0'
+    res['Token'][1000]["address"] = '0xAB2A0e91d75f1c3F1E368F247f168FBB9363172d'
+    res['MemoDB'][1000]["address"] = '0x6Dafc095b8350F4c17fb5765c644F2Af6979219C'
 
     with open(settings.contracts_json, 'w') as f:
         json.dump(res, f, sort_keys=True)
